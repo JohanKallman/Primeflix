25fede92e134cf71bb54ddbf8c33da223e60d180
Petter <petter.berelin@gmail.com>
2022-03-06 21:20:02 +0100

    hej hej

--- Primeflix/Program.cs
+++ Primeflix/Program.cs
@@ -12,6 +12,7 @@ namespace Primeflix
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hej hej");
             var host = CreateHostBuilder(args).Build();
 
             using (var scope = host.Services.CreateScope())
