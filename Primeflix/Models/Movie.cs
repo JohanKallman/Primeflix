0e3da6c030161cf39d4c73798cdaf4556a6a410f
Petter <petter.berelin@gmail.com>
2022-03-06 21:07:54 +0100

    remove year

--- Primeflix/Models/Movie.cs
+++ Primeflix/Models/Movie.cs
@@ -13,7 +13,7 @@ namespace Primeflix.Models
         public int Id { get; set; }
         public string Title { get; set; }
 
-        [Display(Name = "Release Date YEAR")]
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
