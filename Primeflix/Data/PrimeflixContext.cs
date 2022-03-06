320b770ab7d9a17dfbc266f6b2bc0fe87adf1dd9
JohanKallman <johan.kallman@iths.se>
2022-03-06 20:19:35 +0100

    Add project files.

---
+++ Primeflix/Data/PrimeflixContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Primeflix.Models;
+
+namespace Primeflix.Data
+{
+    public class PrimeflixContext : IdentityDbContext
+    {
+        public PrimeflixContext (DbContextOptions<PrimeflixContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Primeflix.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Primeflix/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Primeflix.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
