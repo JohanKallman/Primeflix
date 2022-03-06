3ebb5919de5fd3917b5b1d522bdf46db74164ad4
JohanKallman <johan.kallman@iths.se>
2022-03-06 21:40:41 +0100

    added stuff

---
+++ Primeflix/Models/Stuff.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Primeflix.Models
+{
+    public class Stuff
+    {
+        public string GoodStuff { get; set; }
+    }
+}
