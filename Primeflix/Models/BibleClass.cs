24dfd34e74bfcaaddbc48cb488855cf817e5347c
Petter <petter.berelin@gmail.com>
2022-03-06 21:37:28 +0100

    nya grejor

---
+++ Primeflix/Models/BibleClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Primeflix.Models
+{
+    public class BibleClass
+    {
+        public string Trosa { get; set; }
+
+        public BibleClass()
+        {
+            Trosa = "bra";
+        }
+    }
+}
